36662c9c3c4f6dab1f2fca0e12e50f75a2ad3941
ripabak <faruqirifa2@gmail.com>
2026-05-17 16:31:37 +0700

    fix: ensure NEXT_PUBLIC_API_URL is set and use it directly

--- lib/eden-app.ts
+++ lib/eden-app.ts
@@ -1,13 +1,7 @@
 import type { App } from '@/src/types/app';
 import { treaty } from '@elysia/eden';
 
-if (!process.env.NEXT_PUBLIC_API_URL) {
-  throw new Error('NEXT_PUBLIC_API_URL is not set');
-}
-
-const apiUrl = process.env.NEXT_PUBLIC_API_URL.replace(/^http:\/\//i, 'https://');
-
-export const app = treaty<App>(apiUrl, {
+export const app = treaty<App>(process.env.NEXT_PUBLIC_API_URL!, {
   fetch: {
     credentials: 'include'
   }
